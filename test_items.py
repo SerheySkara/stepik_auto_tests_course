8c8d8f1c03aa476ccc6ade39fafa58373c2b4394
SerheySkara <kupolnd@gmail.com>
2024-11-07 12:17:29 +0200

    Added conftest.py and test_items.py for language-based test

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
+def test_add_to_cart_button(browser):
+    # Открываем страницу товара
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    
+    # Ищем кнопку добавления в корзину
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, "Button 'Add to cart' not found on the page"
 
